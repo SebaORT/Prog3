fb503fbc4121896262f520e935e9cb8baff1d89d
ceciliaB97 <cecilia.belon@hotmail.com>
2021-09-26 17:52:16 -0300

    cambios

--- obligatorioP3/Auxiliar/Facade.cs
+++ obligatorioP3/Auxiliar/Facade.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +7,128 @@ using System.Threading.Tasks;
 
 namespace Auxiliar
 {
-	public class Facade
-	{
-		public static Facade _instance =null;
+    public class Facade
+    {
+        public static Facade _instance = null;
 
-		private FabricaRepositorios _fabricaRepositorios;
+        private FabricaRepositorios _fabricaRepositorios;
 
-		public static Facade Instance { 
-			get {
-				if (_instance == null)
-				{
-					_instance = new Facade();
-				}
+        public static Facade Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Facade();
+                }
 
-				return _instance;
-			}
-		}
+                return _instance;
+            }
+        }
 
-		public Facade()
-		{
-			_fabricaRepositorios = new FabricaRepositorios();
-		}
+        public Facade()
+        {
+            _fabricaRepositorios = new FabricaRepositorios();
+        }
 
-	}
+        #region metodos
+
+        public FabricaRepositorios FabricaRepositorios()
+        {
+            return new FabricaRepositorios();
+        }
+
+        public int AltaMembresia(int cedula, Membresia tipo)
+        {
+            return 0;
+        }
+
+        public int BajaMembresia(int id)
+        {
+            return 0;
+        }
+
+        public int ModificacionMembresia(int id)
+        {
+            return 0;
+        }
+
+        public int BuscarMembresia(int cedula)
+        {
+            return 0;
+        }
+
+        public List<Membresia> ListarMembresias()
+        {
+            return new List<Membresia>();
+        }
+
+        public List<Actividad> ListarActividades()
+        {
+            return new List<Actividad>();
+        }
+
+        public Actividad BuscarActividad(int id)
+        {
+            return new Actividad();
+        }
+
+        public int AltaSocio(int cedula, string nombre, string apellido, DateTime fechaNacimiento)
+        {
+            return 0;
+        }
+
+        public int BajaSocio(int cedula)
+        {
+            return 0;
+        }
+
+        public int ModificarSocio(int cedula, string nombre, string apellido)
+        {
+            return 0;
+        }
+
+        public Socio BuscarSocio(int cedula)
+        {
+            return new Socio();
+        }
+
+        public List<Socio> ListarSocios()
+        {
+            return new List<Socio>();
+        }
+
+        public int IngresoSocioActividad(int cedulaSocio, int idActividad)
+        {
+            return 0;
+        }
+
+        public int AltaUsuario(string email, string contrasenia)
+        {
+            return 0;
+        }
+
+        public int LoginUsuario(string email, string contrasenia)
+        {
+            return 0;
+        }
+
+        public int LogOutUsuario(string email)
+        {
+            return 0;
+        }
+
+        public int PagarMensualidadSocio(int cedulaSocio)
+        {
+            return 0;
+        }
+
+        public bool ExportarInfo()
+        {
+            return true;
+        }
+
+        #endregion metodos
+
+    }
 }
--- obligatorioP3/Datos/RepoUsuario.cs
+++ obligatorioP3/Datos/RepoUsuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -39,7 +40,11 @@ namespace Repositorios
 				}
 				finally
 				{
-					connection.Close();
+					if (connection != null)
+					{
+						if (connection.State == ConnectionState.Open) connection.Close();
+						connection.Dispose();
+					}
 				}
 
 			}
