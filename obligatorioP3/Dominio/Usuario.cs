16d2647414a46c68d2123131668cd8509ffaabd7
ceciliaB97 <cecilia.belon@hotmail.com>
2021-10-11 13:46:20 -0300

    validacion contraseña usuario

--- obligatorioP3/Dominio/Usuario.cs
+++ obligatorioP3/Dominio/Usuario.cs
@@ -10,6 +10,22 @@ namespace Dominio
     {
         public int IdUsuario { get; set; }
         public string Mail { get; set; }
-        public string Password { get; set; }      
+        public string Password { get; set; }
+
+        public bool validarDatosUsuario()
+        {
+            //contraseña de al menos 6 caracteres
+            //que incluyan letras mayúsculas y minúsculas (al menos una de cada una)
+            //dígitos (0 al 9)
+            if (Password.Length >= 6)
+            {
+                if (Password.Any(char.IsUpper) && Password.Any(char.IsLower) && Password.Any(char.IsDigit))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
